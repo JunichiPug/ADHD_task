c4a33910edab8950eb6114cfd53ba0e74689ba52
JunichiPug <kyomotojunichi@gmail.com>
2026-06-08 11:36:39 +0900

    メモ機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_06_000000) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_06_010000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_06_000000) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "done", default: false, null: false
     t.index ["user_id"], name: "index_memos_on_user_id"
   end
 
